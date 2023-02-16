a5200bb565d7d901965e0255f9ca7cabc89b9d8e
kchepishev <125463880+kchepishev@users.noreply.github.com>
2023-02-16 14:02:35 +0200

    test

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "blog" {
 
 }
 
- resource = "aws_security_group_rule" "blog_http_in" {
+resource = "aws_security_group_rule" "blog_http_in" {
 
   type        = "ingress"
   from_port   = 80
