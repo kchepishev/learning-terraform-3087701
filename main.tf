cba3d44aaaa4682298974ccf58a8b879411f9097
kchepishev <125463880+kchepishev@users.noreply.github.com>
2023-02-16 13:41:40 +0200

    create security groups

--- main.tf
+++ main.tf
@@ -43,11 +43,11 @@ resource = "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] 
+  cidr_blocks = ["0.0.0.0"] 
 
  security_group_id = aws_security_group.blog.id
-
-}
+ 
+ }
 
 resource = "aws_security_group_rule" "blog_https_in" {
 
