f7ae142a640fd4f3228485df17e513f28537a8b7
kchepishev <125463880+kchepishev@users.noreply.github.com>
2023-02-16 13:19:46 +0200

    create security groups

--- main.tf
+++ main.tf
@@ -39,12 +39,11 @@ resource "aws_security_group" "blog" {
 }
 
 resource = "aws_security_group_rule" "blog_http_in" {
-
- type        = "ingress"
- from_port   = 80
- to_port     = 80
- protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"] 
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"] 
 
  security_group_id = aws_security_group.blog.id
 
@@ -63,7 +62,7 @@ security_group_id = aws_security_group.blog.id
 }
 
 resource = "aws_security_group_rule" "blog_everything_out" {
-  
+
 type        = "egress"
 from_port   = 0
 to_port     = 0
