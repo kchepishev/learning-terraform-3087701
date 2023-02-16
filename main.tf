d5528564b6b7fba2b12e5487730d0ca11adbc6ff
kchepishev <125463880+kchepishev@users.noreply.github.com>
2023-02-16 14:54:29 +0200

    create security groups

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default"{
+  default = true
+
+}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "allow http and https in. And allow everything out."
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "blog_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  securirty_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group" "blog_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  securirty_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group" "blog_everithyng_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 00
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  securirty_group_id = aws_security_group.blog.id
 }
