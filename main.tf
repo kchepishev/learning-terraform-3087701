6d57bed7c1f7a00664062881256a38c91b263505
kchepishev <125463880+kchepishev@users.noreply.github.com>
2023-02-16 15:20:13 +0200

    create security groups

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_vpc" "default"{
 }
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
 
